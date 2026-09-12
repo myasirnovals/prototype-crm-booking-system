d50027f88b1fbc3bcafcd22479d8ec7a2600a419
Muhamad Yasir Noval <myasirnoval@gmail.com>
2026-09-12 14:31:18 +0700

    bug fix: memperbaiki link template

--- js/config/templates/personal-trainer.template.js
+++ js/config/templates/personal-trainer.template.js
@@ -12,7 +12,7 @@ export const PERSONAL_TRAINER_TEMPLATE = {
   accentColor: "#dc2626", // Dynamic Red / Energetic Crimson
   practitionerTitle: "Certified Personal Trainer / Coach",
   practitionerTitleI18n: "template.personalTrainer.practitionerTitle",
-  demoUrl: "https://prototype-application-personal-trai.vercel.app/app.html",
+  demoUrl: "https://prototype-application-personal-trai.vercel.app/",
 
   pricing: {
     monthly: 99,
